181c6d5633460f330c39fc9d8600d67a0f05c151
Rowayi567 <rowayichtr24@gmail.com>
2026-03-30 23:44:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 import { registerRootComponent } from 'expo';
-import { AppRegistry } from 'react-native';
 import { WebView } from 'react-native-webview';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
 
 function ChinsMobileApp() {
   return (
     React.createElement(View, { style: styles.container },
       React.createElement(WebView, {
-        source: { uri: 'https://chins.app' },
+        source: { uri: 'https://chins-4.vercel.app' },
         style: styles.webview,
         javaScriptEnabled: true,
         domStorageEnabled: true,
         startInLoadingState: true,
         allowsInlineMediaPlayback: true,
+        renderLoading: () => React.createElement(View, { style: styles.loading },
+          React.createElement(ActivityIndicator, { size: 'large', color: '#4BC1A0' })
+        ),
       })
     )
   );
@@ -26,6 +28,12 @@ const styles = StyleSheet.create({
   },
   webview: {
     flex: 1,
+  },
+  loading: {
+    flex: 1,
+    backgroundColor: '#021a16',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
